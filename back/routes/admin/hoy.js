b4ba098beb52b51d8ed185ecea6e898fd5e6fd27
Lucia Belmonte <Lucia.belmonte@hotmail.com.ar>
2023-11-15 20:59:04 -0300

    15-11

--- back/routes/admin/hoy.js
+++ back/routes/admin/hoy.js
@@ -4,11 +4,31 @@ var hoyModel = require('../../models/hoyModel');
 const util = require('util');
 const cloudinary = require('cloudinary').v2;
 const uploader = util.promisify(cloudinary.uploader.upload);
+const destroy = util.promisify(cloudinary.uploader.destroy);
 
 
 router.get('/', async function (req, res, next) {
 
   var hoy = await hoyModel.getHoy();
+
+  hoy = hoy.map(hoy => {
+    if (hoy.img_id) {
+      const imagen = cloudinary.image(hoy.img_id, {
+        width: 100,
+        height: 100,
+        crop: 'fill'
+      });
+      return {
+        ...hoy,
+        imagen
+      }
+    } else {
+      return {
+        ...hoy,
+        imagen: ''
+      }
+    }
+  });
 
   res.render('admin/hoy', {
     layout: 'admin/layout',
@@ -58,9 +78,14 @@ router.post('/agregar', async (req, res, next) => {
     })
   }
 })
-
+/*eliminar una novedad*/
 router.get('/eliminar/:id', async (req, res, next) => {
   var id = req.params.id;
+
+  let hoy = await hoyModel.getHoyById(id);
+if (hoy.img_id) {
+  await (destroy(hoy.img_id));
+}
 
   await hoyModel.deleteHoyById(id);
   res.redirect('/admin/hoy');
@@ -79,21 +104,46 @@ router.get('/modificar/:id', async (req, res, next) => {
   });
 });
 
+/*modificar una novedad*/
 router.post('/modificar', async (req, res, next) => {
   try {
+
+let img_id = req.body.img_original;
+
+let borrar_img_vieja = false;
+
+if (req.body.img_delete === "1") {
+  img_id = null;
+  borrar_img_vieja= true;
+}else {
+  if( req.files && Object.keys(req.files). length > 0) {
+    imagen= req.files.imagen;
+    img_id = (await
+      uploader(imagen.tempFilePath)).public_id;
+      borrar_img_vieja = true;
+  }
+}
+if (borrar_img_vieja && req.body.img_original){
+  await (destroy(req.body.img_original));
+}
+
     let obj = {
       titulo: req.body.titulo,
       Subtitulo: req.body.Subtitulo,
-      Cuerpo: req.body.Cuerpo
+      Cuerpo: req.body.Cuerpo,
+      img_id
     }
+    console.log(req.body);
+
     await hoyModel.modificarHoyById(obj, req.body.id);
     res.redirect('/admin/hoy');
-  }
-  catch (error) {
+
+  } catch (error) {
     console.log(error)
     res.render('admin/modificar', {
       layout: 'admin/layout',
-      error: true, message: 'No se modifico la novedad'
+      error: true,
+      message: 'No se modifico la novedad'
     })
   }
 })
