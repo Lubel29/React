c2407e72fbbadd932d0b4f16bee7bfd3a191c2d5
Lucia Belmonte <Lucia.belmonte@hotmail.com.ar>
2023-11-24 16:35:54 -0300

    24-11

--- back/routes/api.js
+++ back/routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var hoyModel = require('./../models/hoyModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
 
 router.get('/hoy', async function(req,res,next) {
     let hoy = await hoyModel.getHoy();
@@ -27,5 +28,30 @@ router.get('/hoy', async function(req,res,next) {
     
     res.json(hoy);
 });
+
+router.post('/contacto',async(req,res) => {
+    const mail = {
+        to: 'lucia.belmonte17.lb@gmail.com',
+        subject:'Contacto web',
+        html: `${req.body.nombre} se contacto a traves de la web y quiere mas informacion a este correo: ${req.body.email}<br> Además, hizo el siguiente comentario: ${req.body.mensaje} <br> Su tel es: ${req.body.telefono}`
+    }
+
+   const transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
+   });
+
+   await transport.sendMail(mail)
+
+   res.status(201).json({
+    error:false,
+    message: 'Mensaje enviado'
+   });
+
+}); /*cierra post/api*/
 
 module.exports = router;
